f94fb0a429c811808d40e2a92590e193ef58bf35
Davide Belli <davidebelli95@gmail.com>
2017-11-11 14:20:46 +0100

    parsed datasets

---
+++ data/parser.py
@@ -0,0 +1,20 @@
+
+def parse(input,output):
+    # sentences=[]
+    # targets=[]
+    lines=[]
+    with open('./'+input, 'r') as f:
+        for line in f:
+            _,_,target,sentence = line.split("\t")
+            # targets.append(target)
+            # sentences.append(sentence)
+            lines.append(target+"|"+sentence)
+            print(lines[-1])
+    
+    with open('./'+output,'w') as f:
+        for line in lines:
+            f.write(line)
+            
+parse('source_train.tsv','train.txt')
+parse('source_valid.tsv','valid.txt')
+parse('source_test.tsv','test.txt')
