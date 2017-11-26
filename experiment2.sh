8d81595d6cd88210c348adf27105fe563e3c7842
Davide Belli <davidebelli95@gmail.com>
2017-11-26 03:24:02 +0100

    Experiment 2 Ready

--- experiment2.sh
+++ experiment2.sh
@@ -25,8 +25,10 @@ python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.5 --model RNN_TANH -
 echo "--epochs 100 --lr 0.1 --lamb 0 --model RNN_TANH --batch_size 20"
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0 --model RNN_TANH --batch_size 20
 echo "--epochs 800 --lr 5 --lamb 0 --model LSTM --batch_size 100"
-python main_last.py --cuda --plot --epochs 800 --lr 5 --lamb 0 --model LSTM --batch_size 100
+python main_last.py --cuda --plot --epochs 800 --lr 0.5 --lamb 0 --model LSTM --batch_size 100
 echo "--epochs 800 --lr 5 --lamb 1 --model LSTM --batch_size 100"
-python main_last.py --cuda --plot --epochs 800 --lr 5 --lamb 1 --model LSTM --batch_size 100
+python main_last.py --cuda --plot --epochs 800 --lr 0.1 --lamb 1 --model LSTM --batch_size 100
 echo "--epochs 800 --lr 20 --lamb 0.5 --model LSTM --batch_size 20"
-python main_last.py --cuda --plot --epochs 800 --lr 20 --lamb 0.5 --model LSTM --batch_size 20
+python main_last.py --cuda --plot --epochs 800 --lr 0.1 --lamb 0.5 --model LSTM --batch_size 20
+echo "--epochs 100 --lr 0.1 --lamb 0.6 --model LSTM --batch_size 20 --hidden 4"
+python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.5 --model LSTM --batch_size 20
