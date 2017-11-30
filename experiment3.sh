f2eca2b92eb677c45d2249d10d8b810605249552
Davide Belli <davidebelli95@gmail.com>
2017-11-30 03:37:32 +0100

    updated experiment3

--- experiment3.sh
+++ experiment3.sh
@@ -2,6 +2,12 @@
 
 source activate nlp1
 
+echo "--epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_REV --batch_size 20"
+python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_REV --batch_size 20
+echo "--epochs 100 --lr 0.01 --lamb 0.5 --model LSTM --batch_size 20"
+python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.5 --model LSTM --batch_size 20
+echo "--epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_BIDIR --batch_size 20"
+python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_BIDIR --batch_size 20
 echo "--epochs 100 --lr 0.1 --lamb 0.5 --model LSTM_REV --batch_size 20"
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.5 --model LSTM_REV --batch_size 20
 echo "--epochs 100 --lr 0.1 --lamb 0.5 --model LSTM --batch_size 20"
@@ -10,12 +16,8 @@ echo "second experiment --epochs 100 --lr 0.1 --lamb 0.5 --model LSTM --batch_si
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.5 --model LSTM --batch_size 20
 echo "--epochs 100 --lr 0.1 --lamb 0.5 --model LSTM_BIDIR --batch_size 20"
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.5 --model LSTM_BIDIR --batch_size 20
-echo "--epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_REV --batch_size 20"
-python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_REV --batch_size 20
-echo "--epochs 100 --lr 0.01 --lamb 0.5 --model LSTM --batch_size 20"
-python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.5 --model LSTM --batch_size 20
-echo "--epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_BIDIR --batch_size 20"
-python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.5 --model LSTM_BIDIR --batch_size 20
+echo "--epochs 200 --lr 0.002 --lamb 0.5 --model LSTM --batch_size 20"
+python main.py --cuda --plot --epochs 200 --lr 0.002 --lamb 0.5 --model LSTM --batch_size 20
 echo "--last --epochs 1000 --lr 0.1 --lamb 0.5 --model LSTM --batch_size 100"
 python main.py --cuda --plot --last --epochs 1000 --lr 0.1 --lamb 0.5 --model LSTM_REV --batch_size 100
 echo "--last --epochs 1000 --lr 0.1 --lamb 0.5 --model LSTM --batch_size 100"
