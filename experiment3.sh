c7a16e66d667d66dcc840670d7a90f8ef8b8ce4f
Davide Belli <davidebelli95@gmail.com>
2017-11-30 04:42:27 +0100

    updated experiment3

--- experiment3.sh
+++ experiment3.sh
@@ -12,8 +12,8 @@ echo "--epochs 100 --lr 0.1 --lamb 0.1 --model LSTM_REV --batch_size 20"
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.1 --model LSTM_REV --batch_size 20
 echo "--epochs 100 --lr 0.1 --lamb 0.1 --model LSTM --batch_size 20"
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.1 --model LSTM --batch_size 20
-echo "second experiment --epochs 100 --lr 0.1 --lamb 0.1 --model LSTM --batch_size 20"
-python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.1 --model LSTM --batch_size 20
+echo "--epochs 100 --lr 0.01 --lamb 0.2 --model LSTM_REV --batch_size 20"
+python main.py --cuda --plot --epochs 100 --lr 0.01 --lamb 0.2 --model LSTM_REV --batch_size 20
 echo "--epochs 100 --lr 0.1 --lamb 0.1 --model LSTM_BIDIR --batch_size 20"
 python main.py --cuda --plot --epochs 100 --lr 0.1 --lamb 0.1 --model LSTM_BIDIR --batch_size 20
 echo "--epochs 200 --lr 0.002 --lamb 0.1 --model LSTM --batch_size 20"
