1f81a4a3cb24a07cadfebfb1aff557be2a52846f
Davide Belli <davidebelli95@gmail.com>
2017-12-13 18:27:56 +0100

    updated preprocessor.py

--- data/2017_pre/pre_processor.py
+++ data/2017_pre/pre_processor.py
@@ -1,4 +1,6 @@
 import re
+import string
+from nltk.tokenize import TweetTokenizer
 
 def find_emoticons(sentence):
     smile_emoticons_str = r"""
@@ -29,20 +31,27 @@ def find_emoticons(sentence):
 def preprocess(inp, output):
     sentences=[]
     targets=[]
+    printable = set(string.printable)
+    tknzr = TweetTokenizer(reduce_len=True)
     
     with open('./'+inp, 'r') as f:
         for line in f:
             target, tmp = line.strip().split("|_|")
+            tmp = tmp.lower()
+            tmp = "".join(filter(lambda x: x in printable, tmp))
             tmp = re.sub("https?:?//[\w/.]+", "<URL>", tmp)
             tmp = find_emoticons(tmp)
             tmp = re.sub('[\-_"#@(),!*;:.]', " ", tmp)
             tmp = tmp.replace("?", "")
+            # tmp = tmp.replace("/", "")
             tmp = tmp.replace("&", "")
             tmp = tmp.replace("'s", "")
             tmp = tmp.replace("&quot", "")
             tmp = tmp.replace("&amp", "")
             tmp = tmp.replace("'", " ")
             tmp = tmp.replace("\t", " ")
+            # tkn = tknzr.tokenize(tmp)
+            # tmp = " ".join(tkn)
             tmp_list = tmp.split()
             tmp = " ".join(tmp_list)
             targets.append(target)
